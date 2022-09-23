8f2d1a0d26916f53c7fe7d929df177704cb4997f
satyamshete <shetesatyam99@gmail.com>
2022-09-23 17:45:38 +0530

    [Satyam] Aggfregate and Group by

--- EmployeePayRollServices/EmployeePayRollQuery.sql
+++ EmployeePayRollServices/EmployeePayRollQuery.sql
@@ -21,3 +21,14 @@ SELECT * FROM employee_payroll where StartDate between cast ('05-15-2015' as dat
 alter table employee_payroll add Gender char(1) not null default 'M';
 update employee_payroll set Name = 'Shweta' where Id = 3;
 update employee_payroll set Gender = 'F' where Name = 'Shweta';
+----------UC7---------
+select sum(Salary) as TotalSalary from employee_payroll;
+select avg(Salary) as AvarageSalary from employee_payroll;
+select min(Salary) as MinimumSalary from employee_payroll;
+select max(Salary) as MaximumSalary from employee_payroll;
+select count(Salary) as TotalRecords from employee_payroll;
+select sum(Salary) as TotalSalary, Gender from employee_payroll group by Gender;
+select Avg(Salary) as AvarageSalary,  Gender from employee_payroll group by Gender;
+select min(Salary) as MinimumSalary, Gender from employee_payroll group by Gender;
+select max(Salary) as MaximumSalary, Gender from employee_payroll group by Gender;
+select count(Salary) as TotalRecords, Gender from employee_payroll group by Gender;
