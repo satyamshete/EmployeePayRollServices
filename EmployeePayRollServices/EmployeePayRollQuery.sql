267176d4867efad5e918267064cd0ecd8bc0dd32
satyamshete <shetesatyam99@gmail.com>
2022-09-23 17:05:03 +0530

    [Satyam] Select Record with filter from Table

--- EmployeePayRollServices/EmployeePayRollQuery.sql
+++ EmployeePayRollServices/EmployeePayRollQuery.sql
@@ -12,3 +12,8 @@ Create table employee_payroll (
 insert into employee_payroll values('ArunKumar',325684.12,'2018-08-15'),('Dinesh',225684.12,'2015-08-05'),('Dinesh',15863.21,GETDATE());
 -----------UC4--------
  select * from employee_payroll
+ ----------UC5---------
+SELECT * FROM employee_payroll where  name = 'ArunKumar';
+SELECT * FROM employee_payroll where StartDate between '2018-08-15' and '2022-10-15';
+SELECT * FROM employee_payroll where StartDate between '2018-08-15' and GETDATE();
+SELECT * FROM employee_payroll where StartDate between cast ('05-15-2015' as date) and '2022-10-15';
